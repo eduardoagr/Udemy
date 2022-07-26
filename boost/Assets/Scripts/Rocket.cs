3fae3fbb4397abfac663dd95f4323990167f22e4
Eduado Gomez <egomezr@outlook.com>
2022-07-26 03:45:00 +0200

    updated

---
+++ boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,45 @@
+
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody body;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            body.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotating Right");
+        }
+    }
+}
